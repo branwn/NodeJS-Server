b0bcf73c46f5d4085772cfdbc0da587db9905ee3
ziyuan <ziyuan1999@outlook.com>
2022-02-19 20:17:48 +0800

    express simple REST

--- NodeJS/node_express/routes/dishRouter.js
+++ NodeJS/node_express/routes/dishRouter.js
@@ -11,18 +11,44 @@ dishRouter.route('/')
         res.setHeader('Content-Type', 'text/plain');
         next();
     })
-        .get((req, res,next) => {
+    .get((req, res,next) => {
         res.end('Will send all the dishes to you!');
     })
-        .post((req, res,next) => {
+    .post((req, res,next) => {
         res.end('Will add the dishes: ' + req.body.name + ' with details: ' + req.body.description);
     })
-        .put((req, res,next) => {
+    .put((req, res,next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /dishes');
     })
-        .delete((req, res,next) => {
+    .delete((req, res,next) => {
         res.end('Deleting all the dishes');
     });
+
+dishRouter.route('/:dishId')
+    .all((req, res, next) => {
+       res.statusCode = 200;
+       res.setHeader('Content-Type', 'text/plain');
+       next();
+    })
+    .get( (req, res,next) => {
+       res.end('Will send the dish ' + req.params.dishId + ' to you!');
+    })
+    .post( (req, res,next) => {
+       res.statusCode = 403;
+       res.end('POST operation not supported on /dish' + req.params.dishId);
+    })
+    .put((req, res,next) => {
+       res.write('Updating the dish: ' + req.params.dishId + '\n');
+       res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .delete( (req, res,next) => {
+       res.end('Deleting the dish: ' + req.params.dishId );
+    });
+
+
+
+
+
 
 module.exports = dishRouter;
