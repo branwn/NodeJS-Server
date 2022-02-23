fa71e611daae3b2de347b86a380b2a3f2902d1f6
ziyuan <ziyuan1999@outlook.com>
2022-02-23 18:29:39 +0800

    https

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -25,12 +25,24 @@ const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-  console.log('Connected correctly to server');
+    console.log('Connected correctly to server');
 }, (err) => {
-  console.log(err);
+    console.log(err);
 })
 
 var app = express();
+
+
+
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+        return next();
+    } else {
+        // 307 for retry
+        res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    }
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -54,18 +66,18 @@ app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Express Framework and MongoDB
 - ☑️ REST APIs
 - ☑️ User Authentication with Cookies and Sessions
-- User Authentication with Passport
-- OAuth and User Authentication
+- ☑️ User Authentication with Passport
 - HTTPS and Secure Communication
+- OAuth and User Authentication
 - Backend as a Service (BaaS)
 
 
@@ -32,3 +32,18 @@ Express Framework and MongoDB
   ```
 
   
+
+## Generate Private Key
+
+```
+openssl genrsa 1024 > private.key
+```
+
+```
+openssl req -new -key private.key -out cert.csr
+```
+
+```
+openssl x509 -req -in cert.csr -signkey private.key -out certificate.pem
+```
+
